5c8a386354304aad2ae983b0cd24d3e7445ca294
tonmenschen <tonmenschen@gmal.com>
2021-02-11 16:12:55 +0100

    Request JSON timestamp from worldtimeapi.org

--- sync.js
+++ sync.js
@@ -1,52 +1,37 @@
 ﻿const fps = 25;
 var dotBuilder;
 var dotBuilderIntervall;
-var syncIntervall;
+var updateIntervall;
 var leftBeep;
 var rightBeep;
+var timeOffset;
 
-!function init ()  {
+!function init() {
     // console.log(new Date());
+    sync();
     leftBeep = document.getElementById("leftBeep");
     rightBeep = document.getElementById("rightBeep");
     dotBuilder = 0;
     dotBuilderIntervall = setInterval(build, 1000 / fps);
 }();
 
-function build () {
-    if (dotBuilder < fps)
-    {
+function build() {
+    if (dotBuilder < fps) {
         document.getElementById((dotBuilder < 10 ? `0${dotBuilder}` : dotBuilder).toString()).style.visibility = 'visible';
-        document.getElementById((dotBuilder < 10 ? `0${dotBuilder}` : dotBuilder).toString()).style.transform = 'rotate(' + (360 / fps * dotBuilder) +'deg)';
+        document.getElementById((dotBuilder < 10 ? `0${dotBuilder}` : dotBuilder).toString()).style.transform = 'rotate(' + (360 / fps * dotBuilder) + 'deg)';
         dotBuilder++;
     }
     else {
         clearInterval(dotBuilderIntervall);
-        syncIntervall = setInterval(sync, 1000 / fps / 2);
+        updateIntervall = setInterval(update, 1000 / fps / 2);
     }
 }
 
 
-// leftBeep = new sound("left.wav");
-// rightBeep = new sound("right.wav");
-
-// function sound(src) {
-//     this.sound = document.createElement("audio");
-//     this.sound.src = src;
-//     this.sound.setAttribute("preload", "auto");
-//     this.sound.setAttribute("controls", "none");
-//     this.sound.style.display = "none";
-//     document.body.appendChild(this.sound);
-//     this.play = function () {
-//         this.sound.play();
-//     }
-// }
-
-
-function sync() {
+function update() {
     let date = new Date();
     let hrs = date.getHours();
-    let mins = date.getMinutes();   
+    let mins = date.getMinutes();
     let secs = date.getSeconds();
     let msec = date.getMilliseconds();
     let frames = Math.floor(msec / 1000 * fps);
@@ -62,22 +47,20 @@ function sync() {
 
     /* Flash current dot */
     var dot = document.getElementsByClassName("dot");
-    for (var i = 0; i < dot.length; i++)
-    {
+    for (var i = 0; i < dot.length; i++) {
         dot.item(i).style.color = 'white';
     }
     document.getElementById(frames).style.color = 'red';
 
     /* Flash every second */
-    if (frames < 1)
-    {
+    if (frames < 1) {
         document.getElementById("clock").style.borderColor = 'red';
 
         if (secs % 2 == 0) {
             document.getElementById("box").style.left = 'calc(100vw - 5vmin)';
 
             if (leftBeep.duration > 0 && leftBeep.paused) {
-                leftBeep.play();
+                // leftBeep.play();
                 // console.log("Left Played @ " + secs);
             }
         }
@@ -85,13 +68,67 @@ function sync() {
             document.getElementById("box").style.left = '0vw';
 
             if (rightBeep.duration > 0 && rightBeep.paused) {
-                rightBeep.play();
+                // rightBeep.play();
                 // console.log("Right Played @ " + secs);
             }
         }
     }
-    else
-    {
+    else {
         document.getElementById("clock").style.borderColor = 'gray';
+    }
+}
+
+
+function sync() {
+    var request = new XMLHttpRequest();
+
+    // Query Server before actual sync
+    request.open('GET', 'https://worldtimeapi.org/api/ip', true);
+    request.send();
+    if (request.readyState == XMLHttpRequest.DONE && request.status != 200) {
+        console.log("Error " + request.status + " while polling for JSON source: " + request.responseText);
+        return;
+    }
+    console.log("Blind poll finished");
+    var timeRequest;
+    var timeAnswer;
+    var answer;
+    var JSONObject;
+    var timeJSON;
+    var timeDelta;
+    var serverTime;
+    var clientTime;
+    request.open('GET', 'https://worldtimeapi.org/api/ip', true);
+    request.send();
+
+    timeRequest = Date.now();
+    request.onreadystatechange = function () {
+        if (request.readyState == XMLHttpRequest.DONE) {
+            if (request.status == 200) {
+                timeAnswer = Date.now();
+                timeDelta = (timeAnswer - timeRequest);
+                answer = request.responseText;
+                JSONObject = JSON.parse(answer);
+                timeJSON = JSONObject.utc_datetime;
+                serverTime = Date.parse(timeJSON);
+                clientTime = (serverTime + timeDelta / 2);
+                timeOffset = Date.now() - clientTime;
+                console.log("responseText: " + answer);
+                console.log("timeJSON: " + timeJSON);
+                console.log("timeRequest: " + timeRequest);
+                console.log("timeAnswer: " + timeAnswer);
+                console.log("timeDelta: " + timeDelta);
+                console.log("serverTime: " + serverTime);
+                console.log("timeOffset: " + timeOffset);
+                console.log("clientTime: " + clientTime);
+
+                document.getElementById("serverTime").innerHTML = "serverTime: " + serverTime.toString();
+                document.getElementById("clientTime").innerHTML = "clientTime: " + clientTime.toString();
+                document.getElementById("deltaTime").innerHTML = "deltaTime: " + timeOffset.toString();
+            }
+            else {
+                console.log("Error " + request.status + " while polling for JSON source: " + request.responseText);
+            }
+        }
     }
 }
