632a15d955e898c841bef18c5de773c236207c63
tonmenschen <tonmenschen@gmal.com>
2021-02-12 01:19:07 +0100

    FPS switch implemented

--- sync.js
+++ sync.js
@@ -1,4 +1,4 @@
-﻿const fps = 25;
+﻿var fps;
 var dotBuilder;
 var dotBuilderIntervall;
 var updateIntervall;
@@ -6,16 +6,56 @@ var leftBeep;
 var rightBeep;
 var timeOffset;
 
-!function init() {
-    // console.log(new Date());
-    sync();
+init();
+
+function init() {
+    //Start Sync from timeserver ... async
     leftBeep = document.getElementById("leftBeep");
     rightBeep = document.getElementById("rightBeep");
+
+    //Set default FPS
+    changeFps(25);
+}
+
+function reset() {
+    //Stop TC Running
+    clearInterval(updateIntervall);
+    
+    document.getElementById("timecode").innerText = "WA:IT:SY:NC";
+    
+    //Hide Dots
     dotBuilder = 0;
-    dotBuilderIntervall = setInterval(build, 1000 / fps);
-}();
+    var dot = document.getElementsByClassName("dot");
+    for (var i = 0; i < dot.length; i++) {
+        dot.item(i).style.visibility = 'hidden';
+        dot.item(i).style.color = 'white';
+    }
+
+    //Build Clock
+    clearInterval(dotBuilderIntervall);
+    dotBuilderIntervall = setInterval(build, 1200 / fps);
+}
+
+function changeFps(newFps) {
+    fps = newFps;
+ 
+    //(re)sync Time
+    sync();
+
+    //(re)build Clock
+    reset();
+
+    //Highlight selected
+    var buttons = document.getElementsByClassName("button");
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].className = buttons[i].className.replace("button active", "button");
+    }
+    document.getElementById('fps' + newFps).className += " active";
+}
 
 function build() {
+    //Build Dots
+
     if (dotBuilder < fps) {
         document.getElementById((dotBuilder < 10 ? `0${dotBuilder}` : dotBuilder).toString()).style.visibility = 'visible';
         document.getElementById((dotBuilder < 10 ? `0${dotBuilder}` : dotBuilder).toString()).style.transform = 'rotate(' + (360 / fps * dotBuilder) + 'deg)';
@@ -23,6 +63,7 @@ function build() {
     }
     else {
         clearInterval(dotBuilderIntervall);
+        //Start TC Running
         updateIntervall = setInterval(update, 1000 / fps / 2);
     }
 }
@@ -45,7 +86,7 @@ function update() {
     let time = `${hrs}:${mins}:${secs}:${frames}`;
     document.getElementById("timecode").innerText = time;
 
-    /* Flash current dot */
+    /* Flash current frame dot */
     var dot = document.getElementsByClassName("dot");
     for (var i = 0; i < dot.length; i++) {
         dot.item(i).style.color = 'white';
@@ -83,13 +124,13 @@ function sync() {
     var request = new XMLHttpRequest();
 
     // Query Server before actual sync
-    request.open('GET', 'https://worldtimeapi.org/api/ip', true);
-    request.send();
-    if (request.readyState == XMLHttpRequest.DONE && request.status != 200) {
-        console.log("Error " + request.status + " while polling for JSON source: " + request.responseText);
-        return;
-    }
-    console.log("Blind poll finished");
+    // request.open('GET', 'https://worldtimeapi.org/api/ip', true);
+    // request.send();
+    // if (request.readyState == XMLHttpRequest.DONE && request.status != 200) {
+    //     console.log("Error " + request.status + " while polling for JSON source: " + request.responseText);
+    //     return;
+    // }
+    // console.log("Blind poll finished");
     var timeRequest;
     var timeAnswer;
     var answer;
@@ -113,14 +154,14 @@ function sync() {
                 serverTime = Date.parse(timeJSON);
                 clientTime = (serverTime + timeDelta / 2);
                 timeOffset = Date.now() - clientTime;
-                console.log("responseText: " + answer);
-                console.log("timeJSON: " + timeJSON);
-                console.log("timeRequest: " + timeRequest);
-                console.log("timeAnswer: " + timeAnswer);
-                console.log("timeDelta: " + timeDelta);
-                console.log("serverTime: " + serverTime);
-                console.log("timeOffset: " + timeOffset);
-                console.log("clientTime: " + clientTime);
+                // console.log("responseText: " + answer);
+                // console.log("timeJSON: " + timeJSON);
+                // console.log("timeRequest: " + timeRequest);
+                // console.log("timeAnswer: " + timeAnswer);
+                // console.log("timeDelta: " + timeDelta);
+                // console.log("serverTime: " + serverTime);
+                // console.log("timeOffset: " + timeOffset);
+                // console.log("clientTime: " + clientTime);
 
                 document.getElementById("serverTime").innerHTML = "serverTime: " + serverTime.toString();
                 document.getElementById("clientTime").innerHTML = "clientTime: " + clientTime.toString();
