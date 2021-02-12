82450841bdc5c973108ec0df7ac1d475cc0161ef
tonmenschen <tonmenschen@gmal.com>
2021-02-12 17:04:26 +0100

    Status implemented, Re-sync working

--- sync.js
+++ sync.js
@@ -1,174 +1,263 @@
-﻿var fps;
+﻿var settingFps;
+var settingSound;
 var dotBuilder;
 var dotBuilderIntervall;
 var updateIntervall;
-var leftBeep;
-var rightBeep;
-var timeOffset;
+var soundPing; //left sound
+var soundPong; //right sound
+var soundBeep;
+var syncOffset; //Positive Offset --> Systemuhr geht nach
+var pulse;
+var timecode;
+var dots;
+var box;
+var clock;
+var buttonsFps;
+var buttonsSound;
+var readyForNextFrame;
+var previousFrame;
+var statusHTML;
+var offsetHTML;
 
 init();
 
 function init() {
-    //Start Sync from timeserver ... async
-    leftBeep = document.getElementById("leftBeep");
-    rightBeep = document.getElementById("rightBeep");
+    //Initiate HTML Elements
+    soundPing = document.getElementById("soundPing");
+    soundPong = document.getElementById("soundPong");
+    soundBeep = document.getElementById("soundBeep");
+    pulse = document.getElementById("pulse");
+    timecode = document.getElementById("timecode");
+    dots = document.getElementsByClassName("dot");
+    box = document.getElementById("box");
+    clock = document.getElementById("clock");
+    buttonsFps = document.getElementsByClassName("button fps");
+    buttonsSound = document.getElementsByClassName("button sound");
+    statusHTML = document.getElementById("status");
+    offsetHTML = document.getElementById("offset");
 
-    //Set default FPS
+    //Re-trigger flags
+    previousFrame = 99;
+    readyForNextFrame = true;
+
+    //Set default FPS: 25
     changeFps(25);
+
+    //Set delauft Sound: off
+    changeSound("off");
+
+    //Auto Re-Sync every minute
+    setInterval(sync, 60000);
 }
 
 function reset() {
     //Stop TC Running
     clearInterval(updateIntervall);
-    
-    document.getElementById("timecode").innerText = "WA:IT:SY:NC";
-    
+
+    timecode.innerText = "WA:IT:SY:NC";
+
     //Hide Dots
     dotBuilder = 0;
-    var dot = document.getElementsByClassName("dot");
-    for (var i = 0; i < dot.length; i++) {
-        dot.item(i).style.visibility = 'hidden';
-        dot.item(i).style.color = 'white';
+
+    for (var i = 0; i < dots.length; i++) {
+        dots.item(i).style.visibility = 'hidden';
+        dots.item(i).style.color = 'white';
     }
 
     //Build Clock
     clearInterval(dotBuilderIntervall);
-    dotBuilderIntervall = setInterval(build, 1200 / fps);
+    dotBuilderIntervall = setInterval(build, 1200 / settingFps);
 }
 
 function changeFps(newFps) {
-    fps = newFps;
- 
+    settingFps = newFps;
+
     //(re)sync Time
     sync();
 
     //(re)build Clock
     reset();
 
-    //Highlight selected
-    var buttons = document.getElementsByClassName("button");
-    for (var i = 0; i < buttons.length; i++) {
-        buttons[i].className = buttons[i].className.replace("button active", "button");
+    //Highlight selected Fps
+    for (var i = 0; i < buttonsFps.length; i++) {
+        buttonsFps[i].className = buttonsFps[i].className.replace(" active", "");
     }
     document.getElementById('fps' + newFps).className += " active";
 }
 
+function changeSound(newSound) {
+    settingSound = newSound;
+
+    //Highlight selected Sound
+    for (var i = 0; i < buttonsSound.length; i++) {
+        buttonsSound[i].className = buttonsSound[i].className.replace(" active", "");
+    }
+    document.getElementById(newSound).className += " active";
+}
+
 function build() {
     //Build Dots
-
-    if (dotBuilder < fps) {
+    if (dotBuilder < settingFps) {
         document.getElementById((dotBuilder < 10 ? `0${dotBuilder}` : dotBuilder).toString()).style.visibility = 'visible';
-        document.getElementById((dotBuilder < 10 ? `0${dotBuilder}` : dotBuilder).toString()).style.transform = 'rotate(' + (360 / fps * dotBuilder) + 'deg)';
+        document.getElementById((dotBuilder < 10 ? `0${dotBuilder}` : dotBuilder).toString()).style.transform = 'rotate(' + (360 / settingFps * dotBuilder) + 'deg)';
         dotBuilder++;
     }
     else {
         clearInterval(dotBuilderIntervall);
         //Start TC Running
-        updateIntervall = setInterval(update, 1000 / fps / 2);
+        updateIntervall = setInterval(update, 1000 / settingFps / 3);
     }
 }
 
 
 function update() {
-    let date = new Date();
+    let date = new Date(Date.now() + Math.round(syncOffset));
     let hrs = date.getHours();
     let mins = date.getMinutes();
     let secs = date.getSeconds();
     let msec = date.getMilliseconds();
-    let frames = Math.floor(msec / 1000 * fps);
+    let frames = Math.floor(msec / 1000 * settingFps);
 
-    hrs = hrs < 10 ? `0${hrs}` : hrs;
-    mins = mins < 10 ? `0${mins}` : mins;
-    secs = secs < 10 ? `0${secs}` : secs;
-    frames = frames < 10 ? `0${frames}` : frames;
+    if (frames != previousFrame) {
+        hrs = hrs < 10 ? `0${hrs}` : hrs;
+        mins = mins < 10 ? `0${mins}` : mins;
+        secs = secs < 10 ? `0${secs}` : secs;
+        frames = frames < 10 ? `0${frames}` : frames;
 
-    /* Update TC text */
-    let time = `${hrs}:${mins}:${secs}:${frames}`;
-    document.getElementById("timecode").innerText = time;
+        /* Update TC text */
+        let time = `${hrs}:${mins}:${secs}:${frames}`;
+        timecode.innerText = time;
 
-    /* Flash current frame dot */
-    var dot = document.getElementsByClassName("dot");
-    for (var i = 0; i < dot.length; i++) {
-        dot.item(i).style.color = 'white';
-    }
-    document.getElementById(frames).style.color = 'red';
+        /* Flash current frame dot */
+        for (var i = 0; i < dots.length; i++) {
+            dots.item(i).style.color = 'white';
+        }
+        document.getElementById(frames).style.color = 'red';
 
-    /* Flash every second */
-    if (frames < 1) {
-        document.getElementById("clock").style.borderColor = 'red';
+        /* Trigger every Second */
+        if (frames < 1 && readyForNextFrame) {
 
-        if (secs % 2 == 0) {
-            document.getElementById("box").style.left = 'calc(100vw - 5vmin)';
+            //Flash Border
+            clock.style.borderColor = 'red';
+            //Flash Pulse
+            pulse.classList.remove('notransition');
+            pulse.style.opacity = 0;
+            pulse.style.borderWidth = '6vmin';
 
-            if (leftBeep.duration > 0 && leftBeep.paused) {
-                // leftBeep.play();
-                // console.log("Left Played @ " + secs);
+            //Prevent multiple 00 Frame trigers
+            readyForNextFrame = false;
+
+            /* Move Ping-ping box */
+            if (secs % 2 == 0) {
+                box.style.left = 'calc(100vw - 5vmin)';
+
+                // Play left sound
+                switch (settingSound) {
+                    case "off":
+                        break;
+                    case "ping-pong":
+                        if (soundPing.duration > 0 && soundPing.paused) {
+                            soundPing.play();
+                        }
+                        break;
+                    case "beep":
+                        if (soundBeep.duration > 0 && soundBeep.paused) {
+                            soundBeep.play();
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else {
+                box.style.left = '0vw';
+
+                // Play right sound
+                switch (settingSound) {
+                    case "off":
+                        break;
+                    case "ping-pong":
+                        if (soundPong.duration > 0 && soundPong.paused) {
+                            soundPong.play();
+                        }
+                        break;
+                    case "beep":
+                        if (soundBeep.duration > 0 && soundBeep.paused) {
+                            soundBeep.play();
+                        }
+                        break;
+                    default:
+                        break;
+                }
             }
         }
         else {
-            document.getElementById("box").style.left = '0vw';
-
-            if (rightBeep.duration > 0 && rightBeep.paused) {
-                // rightBeep.play();
-                // console.log("Right Played @ " + secs);
-            }
+            clock.style.borderColor = 'gray';
         }
+        previousFrame = frames;
     }
-    else {
-        document.getElementById("clock").style.borderColor = 'gray';
+
+    //Reset Pulse -> readyForNextFrame;
+    if (frames == 23) {
+        pulse.classList.add('notransition');
+        pulse.style.opacity = 1;
+        pulse.style.borderWidth = '3vmin';
+
+        readyForNextFrame = true;
     }
 }
 
 
 function sync() {
-    var request = new XMLHttpRequest();
+    let debugString = "";
+    let request = new XMLHttpRequest();
+    let timeRequest; //Time @ Status 2: HEADERS_RECEIVED -> server starts response
+    let timeAnswer;  //Time @ Status 4: DONE -> JSON time liegt vor
+    let JSONObject;  //Parsed responseText as JSON string
+    let timeJSON;    //Extract UTC-time
+    let timeDelta;   //Time between request and answer
+    let serverTime;  //Parse to UNIX-Time
+    let clientTime;
 
-    // Query Server before actual sync
-    // request.open('GET', 'https://worldtimeapi.org/api/ip', true);
-    // request.send();
-    // if (request.readyState == XMLHttpRequest.DONE && request.status != 200) {
-    //     console.log("Error " + request.status + " while polling for JSON source: " + request.responseText);
-    //     return;
-    // }
-    // console.log("Blind poll finished");
-    var timeRequest;
-    var timeAnswer;
-    var answer;
-    var JSONObject;
-    var timeJSON;
-    var timeDelta;
-    var serverTime;
-    var clientTime;
+    //Show (re)sync Status
+    statusHTML.innerHTML = "&#11044; Syncing";
+    document.getElementById("status").style.color = 'orange';
+
+    //Query Time-Server 
     request.open('GET', 'https://worldtimeapi.org/api/ip', true);
+    debugString += request.readyState + ":" + Date.now() + ",";
     request.send();
 
-    timeRequest = Date.now();
     request.onreadystatechange = function () {
+        debugString += request.readyState + ":" + Date.now() + ",";
+
+        //Store timeRequest when XMLHttpRequest.HEADERS_RECEIVED (request received)
+        if (request.readyState == 2) {
+            timeRequest = Date.now();
+        }
+
+        //Store timeAnswer when XMLHttpRequest.DONE (request finished and response is ready)
         if (request.readyState == XMLHttpRequest.DONE) {
             if (request.status == 200) {
                 timeAnswer = Date.now();
                 timeDelta = (timeAnswer - timeRequest);
-                answer = request.responseText;
-                JSONObject = JSON.parse(answer);
+                JSONObject = JSON.parse(request.responseText);
                 timeJSON = JSONObject.utc_datetime;
                 serverTime = Date.parse(timeJSON);
-                clientTime = (serverTime + timeDelta / 2);
-                timeOffset = Date.now() - clientTime;
-                // console.log("responseText: " + answer);
-                // console.log("timeJSON: " + timeJSON);
-                // console.log("timeRequest: " + timeRequest);
-                // console.log("timeAnswer: " + timeAnswer);
-                // console.log("timeDelta: " + timeDelta);
-                // console.log("serverTime: " + serverTime);
-                // console.log("timeOffset: " + timeOffset);
-                // console.log("clientTime: " + clientTime);
+                clientTime = serverTime + timeDelta * 2; //timeDelta passed since server pulled timestamp, x 2 to compensate for roundtrip
+                syncOffset = clientTime - Date.now();
+                debugString += "JSON :" + serverTime + ",Offset: " + syncOffset;
+                console.log(debugString);
 
-                document.getElementById("serverTime").innerHTML = "serverTime: " + serverTime.toString();
-                document.getElementById("clientTime").innerHTML = "clientTime: " + clientTime.toString();
-                document.getElementById("deltaTime").innerHTML = "deltaTime: " + timeOffset.toString();
+                statusHTML.innerHTML = "&#11044; Sync Ok!";
+                statusHTML.style.color = 'green';
+                offsetHTML.innerHTML = "Offset is " + Math.round(syncOffset) + " ms";
             }
             else {
                 console.log("Error " + request.status + " while polling for JSON source: " + request.responseText);
+                statusHTML.innerHTML = "&#11044; No Sync!";
+                statusHTML.style.color = 'red';
+                offsetHTML.innerHTML = "(using client clock)";
             }
         }
     }
